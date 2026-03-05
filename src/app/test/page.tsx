0fb040cf7bd60ba0bf1f109e5df376cbdd124b69
Rakshitha-abc <rakshitharashu60@gmail.com>
2026-03-05 09:06:09 +0530

    Add static test page to verify routing

---
+++ src/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+        <div className="p-24">
+            <h1 className="text-4xl font-bold">Next.js is Working!</h1>
+            <p className="mt-4">This is a static test page.</p>
+        </div>
+    );
+}
